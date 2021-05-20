d9909051e3b074463a7278d9a4bd0925cc1c5ee8
Emily <cas20037@byui.edu>
2021-05-19 22:20:59 -0600

    wwek5

---
+++ hikes/hikes.js
@@ -0,0 +1,21 @@
+const hikesList = [
+    {
+        name: "Bechler Falls",
+        image: "falls.jpg",
+        distance: "3 miles",
+        difficulty: "Easy",
+    
+    },
+    {
+        name: "Teton Canyon",
+        image: "falls.jpg",
+        distance: "3 miles",
+        difficulty: "Easy", 
+    },
+    {
+        name: "Denanda Falls",
+        image: "falls.jpg",
+        distance: "7 miles",
+        difficulty: "Moderate",
+},
+];
---
+++ hikes/main.js
@@ -0,0 +1,23 @@
+import Hikes from './hikes.js';
+
+const hikes = document.querySelector('.hikes');
+
+hikesList.forEach(hike => {
+    let hikes = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let image = document.createElement("img");
+    let distance = document.createElement("p");
+    let difficulty = document.createElement("p");
+
+    h2.innerHTML = `${hikesList.name}`;
+    image.setAttribute("src", `${hikesList.image}`);
+    distance.innerHTML = `Distance: ${hikesList.distance}`;
+    birthDate.innerHTML = `Date of birth: ${hikesList.difficulty}`;
+    
+    hikesList.append(h2);
+    hikes.append(hike);  
+    hikesList.append(image);
+    hikesList.append(distance);
+    hikesList.append(difficulty);
+
+})
