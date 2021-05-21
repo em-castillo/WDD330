e60a4272ccbc743d31c15ae26f8f167551e769fe
Emily <cas20037@byui.edu>
2021-05-20 22:03:47 -0600

    week 5

--- hikes/hikes.js
+++ hikes/hikes.js
@@ -1,25 +1,38 @@
 const hikesList = [
-    {
-        name: "Bechler Falls",
-        image: "falls.jpg",
-        distance: "3 miles",
-        difficulty: "Easy",
-    
-    },
-    {
-        name: "Teton Canyon",
-        image: "falls.jpg",
-        distance: "3 miles",
-        difficulty: "Easy", 
-    },
-    {
-        name: "Denanda Falls",
-        image: "falls.jpg",
-        distance: "7 miles",
-        difficulty: "Moderate",
-},
+  {
+    name: "Bechler Falls",
+    image: "falls.jpeg",
+    alt: "image of Bechler Falls",
+    distance: "3 miles",
+    difficulty: "Easy",
+    description:
+      "Beautiful short hike along the Bechler river to Bechler Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.",
+  },
+  {
+    name: "Teton Canyon",
+    image: "falls.jpeg",
+    alt: "image of Teton Canyon",
+    distance: "3 miles",
+    difficulty: "Easy",
+    description: "Beautiful short (or long) hike through Teton Canyon.",
+    directions:
+      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Stateline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.",
+  },
+  {
+    name: "Denanda Falls",
+    image: "falls.jpeg",
+    alt: "image of Denanda Falls",
+    distance: "7 miles",
+    difficulty: "Moderate",
+    description:
+      "Beautiful hike through Bechler meadows river to Denanda Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.",
+  },
 ];
 
-export default function Hikes(){
-    return hikesList;
-}
+// export const compu = "compu" //named import ex: import {compu} from './hikes.js'
+
+export default hikesList;
