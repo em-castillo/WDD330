39dd92121b688e5b83611a9141a7dc1741ad97cd
Emily <cas20037@byui.edu>
2021-06-03 18:45:04 -0600

    js

--- hikes/hikes2.js
+++ hikes/hikes2.js
@@ -1,4 +1,4 @@
-import Hikes from './hike2';
+import Hikes from './hike2.js';
 //on load grab the array and insert it into the page
 const myHikes = new Hikes('hikes');
 window.addEventListener('load', () => {
