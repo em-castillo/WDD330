054a6db0aed13ba3e89bf0d6d211afa21dfd66a9
Emily <cas20037@byui.edu>
2021-06-09 16:00:10 -0600

    name

--- swapi.js
+++ swapi.js
@@ -6,5 +6,21 @@ fetch(apiURL)
 
     })
     .then(function (object){
-        console.log(object);
+        // console.log(object);
+        const people = object['people'];
+        const cards = document.querySelector('.cards');
+
+        people.forEach(person => {
+            let card = document.createElement('section');
+            let name = document.createElement('h3');
+            let gender = document.createElement('p');
+            let birth = document.createElement('p');
+            let height = document.createElement('p');
+
+            name.textContent = person.name;
+            console.log(name);
+
+            cards.append(card);
+            card.append(name);
+        })
     })
