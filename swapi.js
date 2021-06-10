428bedcd3e453e60a96e4f466158f055b918da16
Emily <cas20037@byui.edu>
2021-06-10 16:44:45 -0600

    next

--- swapi.js
+++ swapi.js
@@ -31,47 +31,45 @@ fetch(apiURL)
                 card.append(gender);
                 card.append(birth);
                 card.append(height);
-
-
-                function next(e){
-                    const next = results.next;
-                    console.log(next);
-                    // const apiURL = '//swapi.dev/api/people/?page=2';
-                    // fetch(apiURL)
-                    //     .then(function(data){
-                    //         return data.json();
-                    //     })
-                    //     .then(function(object){
-                    //         console.log(object);
-                
-                    //         let results = object['results'];
-                    //         let cards = document.querySelector(".cards");
-                    
-                    //             results.forEach(people => {
-                            
-                    //                 let card = document.createElement('section');
-                    //                 let name = document.createElement('h3');
-                    //                 let gender = document.createElement('p');
-                    //                 let birth = document.createElement('p');
-                    //                 let height = document.createElement('p');
-                                    
-                    
-                    //                 name.textContent = `${people.name}`;
-                    //                 gender.textContent = `Gender: ${people.gender}`;
-                    //                 birth.textContent = `Year of birth: ${people.birth_year}`;
-                    //                 height.textContent = `Height: ${people.height}`;
-                                    
-                    
-                    //                 cards.append(card);
-                    //                 card.append(name);
-                    //                 card.append(gender);
-                    //                 card.append(birth);
-                    //                 card.append(height);
-                    //     })
-                    // })
-                }
                 
         })
     })
+let results = object['results'];
 
+function next(e){
+    const next = results.next;
+    console.log(next);
+    // const apiURL = '//swapi.dev/api/people/?page=2';
+    // fetch(apiURL)
+    //     .then(function(data){
+    //         return data.json();
+    //     })
+    //     .then(function(object){
+    //         console.log(object);
 
+    //         let results = object['results'];
+    //         let cards = document.querySelector(".cards");
+    
+    //             results.forEach(people => {
+            
+    //                 let card = document.createElement('section');
+    //                 let name = document.createElement('h3');
+    //                 let gender = document.createElement('p');
+    //                 let birth = document.createElement('p');
+    //                 let height = document.createElement('p');
+                    
+    
+    //                 name.textContent = `${people.name}`;
+    //                 gender.textContent = `Gender: ${people.gender}`;
+    //                 birth.textContent = `Year of birth: ${people.birth_year}`;
+    //                 height.textContent = `Height: ${people.height}`;
+                    
+    
+    //                 cards.append(card);
+    //                 card.append(name);
+    //                 card.append(gender);
+    //                 card.append(birth);
+    //                 card.append(height);
+    //     })
+    // })
+}
